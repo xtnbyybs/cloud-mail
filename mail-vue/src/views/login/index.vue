1cc3d021faf772ba7e47131d59c168b520bfd083
xtnbyybs <2111803140@qq.com>
2026-01-27 21:46:13 +0800

    Update GitHub link to new URL

--- mail-vue/src/views/login/index.vue
+++ mail-vue/src/views/login/index.vue
@@ -140,7 +140,7 @@
         </el-button>
       </div>
     </el-dialog>
-    <a class="github" href="https://github.com/maillab/cloud-mail">
+    <a class="github" href="http://xtby.xyz">
       <Icon icon="mingcute:github-line" color="#1890ff" width="20" height="20" />
     </a>
   </div>
